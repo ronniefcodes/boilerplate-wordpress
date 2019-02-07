72acb7e7d8d8a841ddfce39e7de7e3d592c6375b
Ronnie Cheung <ronniefcheung@gmail.com>
2019-02-06 22:05:54 -0500

    Added ability to pass theme name (and environment) through npm command

--- webpack.config.js
+++ webpack.config.js
@@ -5,68 +5,71 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const theme = 'boilerplate';
 
-module.exports = {
-  entry: {
-    app: './client/scripts/app.js',
-  },
-  output: {
-    filename: `[name].js`,
-    path: path.resolve(`./wp-content/themes/${theme}/scripts`),
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'client'),
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.(css|scss)$/,
-        include: [
-          path.resolve(__dirname, 'client'),
-        ],
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif|svg|eot|ttf|woff|woff2|otf)(\?.*)?$/,
-        include: [
-          path.resolve(__dirname, 'client'),
-        ],
-        loader: 'file-loader',
-        options: {
-          name: '../media/[name].[ext]'
-        }
-      }
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(
-      [ 'media/*.*', 'scripts/*.*', 'styles/*.*' ],
-      {
-        root: path.resolve(`./wp-content/themes/${theme}`),
-        beforeEmit: true,
-      }
-    ),
-    new CopyWebpackPlugin(
-      [
+module.exports = (env = 'production', argv) => {
+  const theme = argv.theme || 'boilerplate';
+
+  return {
+    entry: {
+      app: './client/scripts/app.js',
+    },
+    output: {
+      filename: `[name].js`,
+      path: path.resolve(`./wp-content/themes/${theme}/scripts`),
+    },
+    mode: env,
+    module: {
+      rules: [
         {
-          from: 'client/media',
-          to: path.resolve(`./wp-content/themes/${theme}/media`),
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'client'),
+          loader: 'babel-loader',
         },
-      ]
-    ),
-    new DefinePlugin({
-      __CLIENT_VERSION__: config.version,
-    }),
-    new MiniCssExtractPlugin({
-      filename: '../styles/[name].css',
-    }),
-  ],
-}
+        {
+          test: /\.(css|scss)$/,
+          include: [
+            path.resolve(__dirname, 'client'),
+          ],
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(jpg|png|gif|svg|eot|ttf|woff|woff2|otf)(\?.*)?$/,
+          include: [
+            path.resolve(__dirname, 'client'),
+          ],
+          loader: 'file-loader',
+          options: {
+            name: '../media/[name].[ext]'
+          }
+        }
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(
+        [ 'media/*.*', 'scripts/*.*', 'styles/*.*' ],
+        {
+          root: path.resolve(`./wp-content/themes/${theme}`),
+          beforeEmit: true,
+        }
+      ),
+      new CopyWebpackPlugin(
+        [
+          {
+            from: 'client/media',
+            to: path.resolve(`./wp-content/themes/${theme}/media`),
+          },
+        ]
+      ),
+      new DefinePlugin({
+        __CLIENT_VERSION__: config.version,
+      }),
+      new MiniCssExtractPlugin({
+        filename: '../styles/[name].css',
+      }),
+    ],
+  };
+};
