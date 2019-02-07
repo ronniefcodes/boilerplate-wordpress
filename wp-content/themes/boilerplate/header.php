1ef10c81255d970b843ba80429832df56ef5c159
Ronnie Cheung <ronniefcheung@gmail.com>
2019-02-06 21:54:02 -0500

    Added basic styling for header and footer

--- wp-content/themes/boilerplate/header.php
+++ wp-content/themes/boilerplate/header.php
@@ -1,2 +1,7 @@
 <header>
+  <div class="header__container">
+    <h1 class="header__title">
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
+    </h1>
+  </div>
 </header>
---
+++ wp-content/themes/boilerplate/partials/footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <div class="footer__container">
+    <div class="footer__image">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/media/logo--wordpress.svg" alt="WordPress Logo" />
+    </div>
+    <div class="footer_text">
+      <small>&copy; Copyright ??? <?php echo date( 'Y' ) ?>. All rights reserved</small>
+    </div>
+  </div>
+</footer>
